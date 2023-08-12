eed0c514fc061b2f0d90832f61c36e84bf45f6de
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-12 11:28:07 +0200

    Prepare for Challenge 1

--- 03-react-basic-and-components/src/index.js
+++ 03-react-basic-and-components/src/index.js
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
