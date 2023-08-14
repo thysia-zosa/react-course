b0d2984f6147a94dd3bf049b62a5adf9507e4313
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-14 19:23:07 +0200

    Complete practice 2

--- 03-react-basic-and-components/src/components/Expense/ExpenseList.js
+++ 03-react-basic-and-components/src/components/Expense/ExpenseList.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 
 function ExpenseList({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState(2020);
+
+  function filterCallback(year) {
+    setFilteredYear(year);
+  }
+
   return (
     <Card className="expense-list">
-      {expenses.map((item, index) => {
-        return (
-          <ExpenseItem
-            key={index}
-            date={item.date}
-            amount={item.amount}
-            title={item.title}
-          />
-        );
-      })}
+      <ExpensesFilter
+        filteredYear={filteredYear}
+        filterCallback={filterCallback}
+      />
+      {expenses
+        .filter((expense) => expense.date.getFullYear() === filteredYear)
+        .map((item, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              date={item.date}
+              amount={item.amount}
+              title={item.title}
+            />
+          );
+        })}
     </Card>
   );
 }
---
+++ 03-react-basic-and-components/src/components/ExpenseFilter/ExpensesFilter.js
@@ -0,0 +1,23 @@
+import "./ExpensesFilter.css";
+
+function ExpensesFilter({ filteredYear, filterCallback }) {
+  function selectChangeHandler(event) {
+    filterCallback(parseInt(event.target.value));
+  }
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={filteredYear} onChange={selectChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensesFilter;
