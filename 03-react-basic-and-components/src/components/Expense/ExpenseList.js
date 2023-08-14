8c7a2901344bb12a2d0a22fa0d16731575ac3375
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-14 19:34:32 +0200

    Learn about derived state

--- 03-react-basic-and-components/src/components/Expense/ExpenseList.js
+++ 03-react-basic-and-components/src/components/Expense/ExpenseList.js
@@ -11,12 +11,17 @@ function ExpenseList({ expenses }) {
     setFilteredYear(year);
   }
 
+  // const years = [2019, 2020, 2021, 2022].filter(
+  //   (year) => year !== filteredYear
+  // );
+
   return (
     <Card className="expense-list">
       <ExpensesFilter
         filteredYear={filteredYear}
         filterCallback={filterCallback}
       />
+      {/* <p>Data for years ${years[0]}, ${years[1]} & ${years[2]} is hidden.</p> */}
       {expenses
         .filter((expense) => expense.date.getFullYear() === filteredYear)
         .map((item, index) => {
