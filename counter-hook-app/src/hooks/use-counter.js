41e95ed9362b5238d79fc27f3bf2447645e9d9b8
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-22 11:07:57 +0200

    Improve hook

--- counter-hook-app/src/hooks/use-counter.js
+++ counter-hook-app/src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (adding = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (adding) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [adding]);
 
   return counter;
 };
