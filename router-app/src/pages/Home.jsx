643196565cf596b910a2a121c2e40cfaefe5ad56
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-28 17:28:33 +0200

    About navigation between pages

--- router-app/src/pages/Home.jsx
+++ router-app/src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>My Home Page</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>.
         {/* NOT: Go to <a href="/products">the list of products</a>. */}
+      </p>
+      <p>
+        {/* In this case you should use a link, but for knowledge's sake */}
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
