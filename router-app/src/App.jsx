d8efeddfb484654851cdab1b2e0b3a4b04b3085a
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-28 16:46:28 +0200

    Add routes

--- router-app/src/App.jsx
+++ router-app/src/App.jsx
@@ -1,5 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ router-app/src/pages/Products.jsx
@@ -0,0 +1,5 @@
+const ProductsPage = () => {
+  return <h1>My Products</h1>;
+};
+
+export default ProductsPage;
