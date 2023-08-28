57e5aac67aabc89e1d09087eac1578af2d8ac4a5
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-28 17:11:18 +0200

    Add error page

--- router-app/src/App.jsx
+++ router-app/src/App.jsx
@@ -7,6 +7,7 @@ import {
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 /*
 
@@ -26,6 +27,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
