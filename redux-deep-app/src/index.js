a40babb79554be746f573b4949c0d3de4118afd1
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-28 13:00:59 +0200

    Start redux deep app

---
+++ redux-deep-app/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import { StrictMode } from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<StrictMode><App /></StrictMode>);
