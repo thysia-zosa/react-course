abef46ea1e3d15a5818adb5cd0fb0273a4f65505
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-30 17:25:10 +0200

    About json

--- big-routing-application/frontend/src/pages/Events.jsx
+++ big-routing-application/frontend/src/pages/Events.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
@@ -21,9 +21,12 @@ export async function loader() {
   const response = await fetch("http://192.168.1.199:8080/events");
 
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+
+    // With this, no need for JSON.parse in Error.jsx
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     return response;
   }
