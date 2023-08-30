6025002fd5565333dfb9ee750ea791792e1a3f97
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-08-30 11:48:45 +0200

    Add id output in events

--- big-routing-application/frontend/src/pages/EventDetail.jsx
+++ big-routing-application/frontend/src/pages/EventDetail.jsx
@@ -1,5 +1,15 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 const EventDetailPage = () => {
-  return <h1>Event Detail Page</h1>;
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Event Detail Page</h1>
+      <p>{params.eventId}</p>
+    </Fragment>
+  );
 };
 
 export default EventDetailPage;
