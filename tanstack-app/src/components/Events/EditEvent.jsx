385007bd3cf2058e1ec7088045944909dee6c2ff
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-09-07 13:10:07 +0200

    Add edit functionality

--- tanstack-app/src/components/Events/EditEvent.jsx
+++ tanstack-app/src/components/Events/EditEvent.jsx
@@ -1,20 +1,57 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "../../utils/http.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const params = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { id: params.id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
 
   function handleSubmit(formData) {}
 
   function handleClose() {
-    navigate('../');
+    navigate("../");
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className="center">
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
+  if (isError) {
+    console.log(isError, error);
+    content = (
+      <>
+        <ErrorBlock
+          title="Failed to load event"
+          message={error.info?.message ?? "Failed to fetch event."}
+        />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +59,8 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
-    </Modal>
-  );
+    );
+  }
+
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
