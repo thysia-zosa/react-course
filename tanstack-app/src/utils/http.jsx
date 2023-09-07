bac25637aa65129ffa5d1b4d57464c7d9503467c
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-09-07 10:40:37 +0200

    Add image fetcher

--- tanstack-app/src/utils/http.jsx
+++ tanstack-app/src/utils/http.jsx
@@ -36,3 +36,20 @@ export async function createEvent(eventData) {
 
   return event;
 }
+
+export async function fetchImages({ signal }) {
+  const response = await fetch("http://localhost:3000/events/images", {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the images.");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
