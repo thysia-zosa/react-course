1f071b38c962da99fcb6d4b8e21ec8bce620bbb3
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-09-12 13:54:43 +0200

    Use different animation states

--- real-animation-app/src/components/Modal.jsx
+++ real-animation-app/src/components/Modal.jsx
@@ -1,14 +1,31 @@
-import { createPortal } from 'react-dom';
+import { motion } from "framer-motion";
+import { createPortal } from "react-dom";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        variants={{
+          hidden: {
+            opacity: 0,
+            y: 200,
+          },
+          visible: {
+            opacity: 1,
+            y: 0,
+          },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
