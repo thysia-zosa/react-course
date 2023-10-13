27799dc943fc765b43a06b8aba7adc48f593a1ad
Jonas Tochtermann <thysia.zosa@gmail.com>
2023-10-13 10:49:02 +0200

    How to use types

---
+++ start-typing/basics.ts
@@ -0,0 +1,13 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 11;
+
+let userName: string = '5';
+
+let isInstructor: boolean = true;
